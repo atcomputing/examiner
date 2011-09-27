c9da8c8d126826e35643b84a69e7051c35ca15a0
Martijn Brekhof <m.brekhof@gmail.com>
2011-09-27 21:51:26 +0200

    Added HTTP client to retrieve exam

--- src/nl/atcomputing/lpic1examtrainer/RetrieveExamQuestions.java
+++ src/nl/atcomputing/lpic1examtrainer/RetrieveExamQuestions.java
@@ -1,50 +1,58 @@
 package nl.atcomputing.lpic1examtrainer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.IntentService;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class RetrieveExamQuestions extends IntentService {
-	private static final int NOTIFICATION_ID = 1;
-	  /** 
-	   * A constructor is required, and must call the super IntentService(String)
-	   * constructor with a name for the worker thread.
-	   */
-	  public RetrieveExamQuestions() {
-	      super("RetrieveExameQuestionsService");
-	  }
-
-	  /**
-	   * The IntentService calls this method from the default worker thread with
-	   * the intent that started the service. When this method returns, IntentService
-	   * stops the service, as appropriate.
-	   */
-	  @Override
-	  protected void onHandleIntent(Intent intent) {
-	      // Normally we would do some work here, like download a file.
-	      // For our sample, we just sleep for 5 seconds.
-	      long endTime = System.currentTimeMillis() + 5*1000;
-	      while (System.currentTimeMillis() < endTime) {
-	          synchronized (this) {
-	              try {
-	                  wait(endTime - System.currentTimeMillis());
-	              } catch (Exception e) {
-	              }
-	          }
-	      }
-	      
-	      // Notify user we completed download
-	      String ns = Context.NOTIFICATION_SERVICE;
-	      NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
-	      Notification notification = new Notification(R.drawable.icon, getText(R.string.ticker_text),
-	    	        System.currentTimeMillis());
-	    	Intent notificationIntent = new Intent(this, RetrieveExamQuestions.class);
-	    	PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-	    	notification.setLatestEventInfo(this, getText(R.string.notification_title),
-	    	        getText(R.string.notification_message), pendingIntent);
-	    	mNotificationManager.notify(NOTIFICATION_ID, notification);
-	  }
+	private String examURL;
+	
+	public RetrieveExamQuestions() {
+		super("RetrieveExameQuestionsService");
 	}
+
+	/**
+	 * The IntentService calls this method from the default worker thread with
+	 * the intent that started the service. When this method returns, IntentService
+	 * stops the service, as appropriate.
+	 */
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		//examURL = "http://www.watbenjedan.nl/";
+		examURL = "http://10.0.0.1/test.txt";
+		try {
+			String result = fetchExam(examURL);
+			Log.d(this.getClass().getSimpleName(), result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private String fetchExam(String urlString) throws MalformedURLException, IOException {
+		String line = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		
+        HttpGet request = new HttpGet(urlString);
+        HttpResponse response = httpClient.execute(request);
+        InputStream is = response.getEntity().getContent();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		 while ((line = reader.readLine()) != null)
+	      {
+	        stringBuilder.append(line + "\n");
+	      }
+		return stringBuilder.toString();
+    }
+
+}
