f5cf97ada891ba297ca720ff073773fcb18b234d
Martijn Brekhof <m.brekhof@gmail.com>
2012-01-03 17:22:20 +0100

    Renamed ShowScore to ShowScoreActivity

---
+++ src/nl/atcomputing/examtrainer/ShowScoreActivity.java
@@ -0,0 +1,53 @@
+package nl.atcomputing.examtrainer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * @author martijn brekhof
+ *
+ */
+
+public class ShowScoreActivity extends Activity {
+	private String KEY = "showscore-key";
+	private ShowScoreView showScoreView;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.show_score);
+
+        showScoreView = (ShowScoreView) findViewById(R.id.show_score);
+        showScoreView.setTextView((TextView) findViewById(R.id.show_score_text));
+
+        if (savedInstanceState == null) {
+        	//Ah we are a new activity. We should
+        	//calculate the score and show it
+        } else {
+            Bundle map = savedInstanceState.getBundle(KEY);
+            if (map != null) {
+            	showScoreView.restoreState(map);
+            } else {
+            	showScoreView.setMode(ShowScoreView.PAUSE);
+            }
+            showScoreView.setMode(ShowScoreView.RUNNING);
+        }
+    }
+	
+	@Override
+	protected void onRestart() {
+		showScoreView.setMode(ShowScoreView.RUNNING);
+	}
+
+    @Override
+    protected void onStop() {
+        super.onPause();
+        showScoreView.setMode(ShowScoreView.PAUSE);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBundle(KEY, showScoreView.saveState());
+    }
+}
